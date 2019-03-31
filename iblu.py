a79f02f379a0aa6c978d879aac9762738174d853
sbiego <forsesim@anche.no>
2019-03-31 23:57:23 +0200

    starting work for aur package

--- iblu.py
+++ iblu.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, re, math
+import sys, re, math, getpass
 # import getpass                        #useful for unit systemd
 version = "0.8"
 
@@ -18,7 +18,7 @@ state['current_info'] = str(state['current_pc']) + '% (' + str(state['current'])
 
 help = "Intel Black Light Util · v" + version + "\n\n\t0-100\t\tsets backlight to the given percentage\n\ti (inc)\t\tincreases backlight by a step, optionally add a number to custom percentage (default is " + str(shift_std_pc) + "%)\n\td (dec)\t\tdecreases backlight, optionally add a number to custom percentage (default is " + str(shift_std_pc) + "%)\n\tc (curr)\t\tshows the current\n\tv\t\tshows verbose output\n\tV\t\tshows very verbose output (for debug)\n\tOFF\t\tturns off backlight (use with a grain of salt)\n\nexample: iblu i30\t#increases of 30% the current backlight (30 is optional)"
 
-unit = "[Unit]\nDescription=Intel BackLight Util, changes owner of /sys/class/blacklight/intel_blacklight/brightness\n\n[Service]\nExecStart=/usr/bin/chown USER:wheel /sys/class/backlight/intel_backlight/brightness\n\n[Install]\nWantedBy=multi-user.target\n"
+unit = "[Unit]\nDescription=Intel BackLight Util, changes owner of /sys/class/blacklight/intel_blacklight/brightness\n\n[Service]\nExecStart=/usr/bin/chown " + getpass.getuser() + ":wheel /sys/class/backlight/intel_backlight/brightness\n\n[Install]\nWantedBy=multi-user.target\n"
 
 #### todo
 ### FARE UNA REGEX ALL'INIZIO E POI LE SOTTOSELEZIONI CON LE PIU SEMPLICI (utile per parametri giusti e no e semplificare regex)
@@ -68,6 +68,7 @@ def increase(percentage=shift_std_pc):
 if(len(sys.argv) == 2):                                              ## getting parameters if exist
     option = sys.argv[1]
 
+    # percentage set
     if(re.search(r"^[v|V]{0,1}[100]{1}$|^[100]{1}[v|V]{0,1}$|^[v|V]{0,1}[0-9]{1,2}$|^[0-9]{1,2}[v|V]{0,1}$", option)):  ## percentage
         input_number = re.findall(r"100|[0-9]{0,2}", option)
         if(input_number[0]):
@@ -78,40 +79,53 @@ if(len(sys.argv) == 2):                                              ## getting 
             debug = True if(re.search(r"V", option)) else False
             verboseOut(debug)
 
+    # decrease 1
     if(re.search(r"^[v|V]{0,1}d$|^d[v|V]{0,1}$|^[v|V]{0,1}dec$|^dec[v|V]{0,1}$", option)):                                  ## decrease bl by a step
         decrease()
         if(re.search(r"[v|V]{1}", option)):                                                 ## verbose
             debug = True if(re.search(r"V", option)) else False
             verboseOut(debug)
 
+    # 2 decrease
     if(re.search(r"^d[0-9]{1,2}$|^dec[0-9]{1,2}$|^[0-9]{1,2}d$|^[0-9]{1,2}dec$", option)):                               ## dec bl by percentage
         decrease(int(re.findall(r"[0-9]{1,2}", option)[0]))
         if(re.search(r"[v|V]{1}", option)):                                                 ## verbose
             debug = True if(re.search(r"V", option)) else False
             verboseOut(debug)
 
+    # increase 1
     if(re.search(r"^i$|^inc$", option)):                                       ## increase bl by a step
         increase()
         if(re.search(r"[v|V]{1}", option)):                                                 ## verbose
             debug = True if(re.search(r"V", option)) else False
             verboseOut(debug)
 
+    # 2 increase
     if(re.search(r"^i[0-9]{1,2}$|^inc[0-9]{1,2}$|^[0-9]{1,2}i$|^[0-9]{1,2}inc$", option)):                                ## inc bl by percentage
         increase(int(re.findall(r"[0-9]{1,2}", option)[0]))
         if(re.search(r"[v|V]{1}", option)):                                                 ## verbose
             debug = True if(re.search(r"V", option)) else False
             verboseOut(debug)
     
+    # current info
     if(re.search(r"^c$|^curr$", option)):                                  ## see current bl
         state['invalid_option'] = False
         debug = True if(re.search(r"V", option)) else False
         verboseOut(debug)
     
-    if(re.search(r"^OFF$", option)):                                  ## see current bl
+    # turns off
+    if(re.search(r"^OFF$", option)):                                  
         state['invalid_option'] = False
         state['new'] = 0
         debug = True if(re.search(r"V", option)) else False
         verboseOut(debug)
+    
+    # install unit systemd file
+    if(re.search(r"^--install$", option)):                                  
+        state['invalid_option'] = False
+        print(unit) 
+        # debug = True if(re.search(r"V", option)) else False
+        # verboseOut(debug)
 
 if(state['invalid_option']):
     print(help)
